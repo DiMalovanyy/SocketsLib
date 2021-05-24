9b410f08f2b67aa661f98139295f0991eaa2988e
Dmytro Malovanyy <dmitrymalovanyy@gmail.com>
2021-05-24 22:52:02 +0300

    binary encoding

--- Voting/utils/VoteEncoding.h
+++ Voting/utils/VoteEncoding.h
@@ -9,4 +9,7 @@
 size_t Encode(const VoteInfo *v, uint8_t *outBuf, const size_t bufSize);
 bool Decode(uint8_t *inBuf, const size_t mSize, VoteInfo *v);
 
+size_t EncodeB(VoteInfo *v, uint8_t *outBuf, size_t bufSize);
+bool DecodeB(uint8_t *inBuf, size_t mSize, VoteInfo *v);
+
 #endif
